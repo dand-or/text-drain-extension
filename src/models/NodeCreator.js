5008dde4c089c96bff449f489a9fd83df8c134dc
d-ando <moruze.orange@gmail.com>
2020-03-07 17:49:30 +0900

    scroll iimplement

--- src/models/NodeCreator.js
+++ src/models/NodeCreator.js
@@ -1,8 +1,9 @@
-export class NodeCreator{
+export class NodeCreator {
   static SHADOW_ROOT_ID = "#app";
-  static MAIN_TEMPLATE_ID = "#main_component_template"
-  static BUTTON_TEMPLATE_ID = "#btn_component_template"
-  static COPIED_DIV_ID = "#copied"
+  static MAIN_TEMPLATE_ID = "#main_component_template";
+  static BUTTON_TEMPLATE_ID = "#btn_component_template";
+  static NAV_TEMPLATE_ID = "#nav_component_template";
+  static COPIED_DIV_ID = "#copied";
   static MAIN_TEMPLATE_STYLE = `
   *:focus {
     outline: none;
@@ -41,12 +42,13 @@ export class NodeCreator{
                 -15px -15px 40px #ffffff;
   }`;
 
-  constructor(items){
+  constructor(items) {
     this._items = items;
     this._appElement = document.querySelector(NodeCreator.SHADOW_ROOT_ID);
-    this._shadowRoot = this._appElement.attachShadow({mode:"open"});
+    this._shadowRoot = this._appElement.attachShadow({ mode: "open" });
     this._mainTemplate = document.querySelector(NodeCreator.MAIN_TEMPLATE_ID);
     this._btnTemplate = document.querySelector(NodeCreator.BUTTON_TEMPLATE_ID);
+    this._navTemplate = document.querySelector(NodeCreator.NAV_TEMPLATE_ID);
     this._copiedElement = document.querySelector(NodeCreator.COPIED_DIV_ID);
   }
 
@@ -54,50 +56,73 @@ export class NodeCreator{
     if (!this._items.length) this._shadowRoot.textContent = "no items...";
 
     this._items.map(item => {
-        const key = Object.keys(item)[0];
-        const cloneMain = document.importNode(this._mainTemplate.content, true);
-        const subtitle = cloneMain.querySelector("h2");
-        subtitle.textContent = key;
+      const key = Object.keys(item)[0];
 
-        const buttons = cloneMain.querySelector("div.buttons");
-        item[key].map(txt => {
-          const cloneButton = document.importNode(this._btnTemplate.content,true);
-          const button = cloneButton.querySelector("button");
-          button.textContent = txt;
+      const cloneNavItem = document.importNode(this._navTemplate.content, true);
+      const anchor = cloneNavItem.querySelector("li a");
+      anchor.setAttribute("href", "#" + key);
+      anchor.textContent = key;
+      anchor.addEventListener("click", () => {
+        this._shadowRoot
+          .querySelector("#" + key)
+          .scrollIntoView({ block: "center", inline: "center", behavior: "smooth" });
+      });
+      document.querySelector("nav ul").appendChild(cloneNavItem);
 
-          const copyAction = () => {
-            navigator.clipboard.writeText(txt).then(() =>{
+      const cloneMain = document.importNode(this._mainTemplate.content, true);
+      const subtitle = cloneMain.querySelector("h2");
+      subtitle.textContent = key;
+      subtitle.setAttribute("id", key);
+
+      const buttons = cloneMain.querySelector("div.buttons");
+      item[key].map(txt => {
+        const cloneButton = document.importNode(
+          this._btnTemplate.content,
+          true
+        );
+        const button = cloneButton.querySelector("button");
+        button.textContent = txt;
+
+        const copyAction = () => {
+          navigator.clipboard.writeText(txt).then(
+            () => {
               // this._copiedElement.classList.remove("show-copied");
-              this._copiedElement.animate([
-                {
-                  opacity: 0,
-                  width: "100%",
-                  zIndex: -1
-                },
-                {
-                  opacity: 1,
-                  width: "110%",
-                  zIndex: 1000
-                },
-                {
-                  opacity: 0,
-                  width: "300%",
-                  zIndex: -1
-                }
-              ], 600, "ease-in");
-            }, () => {
+              this._copiedElement.animate(
+                [
+                  {
+                    opacity: 0,
+                    width: "100%",
+                    zIndex: -1
+                  },
+                  {
+                    opacity: 1,
+                    width: "110%",
+                    zIndex: 1000
+                  },
+                  {
+                    opacity: 0,
+                    width: "300%",
+                    zIndex: -1
+                  }
+                ],
+                600,
+                "ease-in"
+              );
+            },
+            () => {
               alert("copy failed. please retry.");
-            });
-          }
-          button.addEventListener("click", copyAction);
+            }
+          );
+        };
+        button.addEventListener("click", copyAction);
 
-          buttons.appendChild(button);
-        });
-        
-        const style = document.createElement("style");
-        style.textContent = NodeCreator.MAIN_TEMPLATE_STYLE;
-        this._shadowRoot.appendChild(style);
-        this._shadowRoot.appendChild(cloneMain);
+        buttons.appendChild(button);
+      });
+
+      const style = document.createElement("style");
+      style.textContent = NodeCreator.MAIN_TEMPLATE_STYLE;
+      this._shadowRoot.appendChild(style);
+      this._shadowRoot.appendChild(cloneMain);
     });
   }
-};
+}
