86b39ffc19a80f8b431d3a1dec8636c1ff40d95a
d-ando <moruze.orange@gmail.com>
2020-03-07 12:49:40 +0900

    cache text to chrome local storage

--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,37 @@
-import './popup.css';
+import "./popup.css";
 
 (() => {
+  const CHROME_STORAGE_KEY = "items";
   const testDom = document.querySelector("#test");
+
+  const storeItems = (value, cb) => {
+    chrome.storage.local.set({ [CHROME_STORAGE_KEY]: value }, () => {
+      cb(value);
+    });
+    console.log("stored");
+  };
+
+  const restoreItems = (cb) => {
+    console.log("restored");
+    chrome.storage.local.get([CHROME_STORAGE_KEY], (value) => { 
+      cb(value[CHROME_STORAGE_KEY]); 
+    });
+  }
+
+  const drawDom = (items) => {
+    testDom.textContent = JSON.stringify(items);
+  }
 
   chrome.runtime.sendMessage(
     {
-      type: 'FROM_POPUP',
+      type: "FROM_POPUP",
       payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
+        message: "I am from text-drain-extension Popup."
+      }
     },
     response => {
-      console.log(response.items);
-      testDom.textContent = JSON.stringify(response.items);
+      const items = response.items;
+      items.length ? storeItems(response.items, drawDom) : restoreItems(drawDom);
     }
   );
 })();
